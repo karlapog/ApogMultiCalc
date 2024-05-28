2572d271ee22334a743948eee740c5536f5e1540
MatundingCalculator <zianwayne14@gmail.com>
2024-05-28 20:33:42 +0800

    Initial commit

---
+++ app/src/main/java/com/example/apogmulticalc/Buttons.java
@@ -0,0 +1,13 @@
+package com.example.apogmulticalc;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Buttons extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buttons);
+    }
+}
